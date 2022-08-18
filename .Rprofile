cff140458be34eb26628cc905052f974f1e313eb
Stephan Schiffels <stephan.schiffels@mac.com>
2022-08-18 11:40:17 +0000

    Initial commit

---
+++ .Rprofile
@@ -0,0 +1,4 @@
+old_path <- Sys.getenv("PATH")
+wd <- getwd()
+bindir <- paste0(wd, "/bin")
+Sys.setenv(PATH = paste(old_path, bindir, sep = ":"))
---
+++ setup.sh
@@ -0,0 +1,15 @@
+# Downloading poseidon-trident
+mkdir -p bin
+cd bin
+wget https://github.com/poseidon-framework/poseidon-hs/releases/latest/download/trident-Linux
+
+# Rename it to `trident` for easier handling, and make it executable:
+mv trident-Linux trident
+chmod u+x trident
+
+# Add to path
+
+
+
+
+
