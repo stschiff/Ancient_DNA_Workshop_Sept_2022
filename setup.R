443871f08441a8a7d9d8efb599a2b72e01fc0cfb
Stephan Schiffels <stephan.schiffels@mac.com>
2022-08-31 08:09:40 +0000

    added some setup scripting

--- setup.R
+++ setup.R
@@ -1,1 +1,10 @@
-# install.packages("reticulate")
+if(!require("reticulate")) install.packages("reticulate")
+
+# Install Miniconda within the project directory
+wd <- getwd()
+mc_path <- paste0(wd, "/miniconda")
+install_miniconda(path = mc_path)
+
+# In the terminal:
+conda install samtools -c bioconda
+
