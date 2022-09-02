b2b6f94dfddca22b7131a0876e9a4221d4ab0057
Stephan Schiffels <stephan_schiffels@mac.com>
2022-09-02 22:44:02 +0200

    Added Sex-determination exercise.

--- working/prepare_sexdet_data.sh
+++ working/prepare_sexdet_data.sh
@@ -2,4 +2,16 @@
 
 scp daghead1:/mnt/archgen/MICROSCOPE/eager_outputs/2022-03-04-romania_sancrai/sex_determination/SexDet.txt /tmp
 
-awk -v OFS="\t" '{$1="SAMPLE" NR; print $1, $2, $3, $4, $5, $6, $7}' /tmp/SexDet.txt > data/SexDet.txt
+awk -v OFS="\t" '{
+  if(NR == 1) {
+    $1 = "Sample";
+    $2 = "TotalAut";
+    $3 = "TotalX";
+    $4 = "TotalY";
+  }
+  else {
+    $1="SAMPLE" NR
+  };
+  if(NR == 1 || ($5 > 1000 && ($8 < 0.95 && $8 > 0.7 || $8 < 0.45))) print $1, $2, $3, $4, $5, $6, $7
+   
+}' /tmp/SexDet.txt > data/SexDet.txt
